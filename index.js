a77d81ade29f7d1efa4cf605c8eefa4a6cf715f3
Audi Hsu <audihsu@gmail.com>
2018-06-23 16:47:04 +0000

    modify motion, orientation, geolocation to web type

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ var app = express();
 var mpu9250 = require("mpu9250");
 var SerialPort = require("serialport");
 
-configs.mpu.address = parseInt(configs.mpu.address, 16);
+if (typeof configs.mpu.address === "string")
+  configs.mpu.address = parseInt(configs.mpu.address, 16);
 var mpu = new mpu9250(configs.mpu);
 
 var serialport = new SerialPort(configs.gps.device, { baudRate: configs.gps.baudrate });
@@ -49,32 +50,64 @@ function parseNMEA(buf) {
   // console.log("checksum: ", checksum, "count_checksum: ", checksumNMEA(data));
   if (checksum === checksumNMEA(data) && data.indexOf("GPGGA") > -1) {
     var arr = data.split(",");
-    arr.push(date_string_get(new Date()));
-    return arr;
+    var lat = arr[2], lon = arr[4], alt = arr[9];
+    if (lat === "" || lon === "" || alt === "")
+      return false;
+
+    var date = date_string_get(new Date());
+    var geo = {
+      latitude: lat,
+      longitude: lon,
+      altitude: alt,
+      number_satellites: arr[7],
+      hdop: arr[8],
+      time: date
+    };
+    return geo;
   }
 
   return false;
 }
 
 function saveNMEA(data) {
-  fs.appendFileSync("./data/geo.log", data.join(",") + "\n");
+  // fs.appendFileSync("./data/geo.log", data.join(",") + "\n");
+  fs.appendFileSync("./data/geo.log", JSON.stringify(data) + "\n");
 }
 
 function mpu_start() {
   if (mpu.initialize()) {
-    setInterval(reading, 17 /* 60 Hz */);
+    setInterval(mpu_reading, 17 /* 60 Hz */);
   }
   else {
     console.log("mpu9255 initialization failed ...");
   }
 }
 
-function reading() {
-  var m9 = mpu.getMotion9();
-  // console.log('MPU VALUE : ', m9);
-  // console.log('Temperature : ' + mpu.getTemperatureCelsius());
-  m9.push(date_string_get(new Date()));
-  fs.appendFileSync("./data/acc.log", m9.join(",") + "\n");
+var ACCEL_DIVIDERS = [ 16384, 8192, 4096, 2048 ];
+var GYRO_DIVIDERS = [ 131.0, 65.5, 32.8, 16.4 ];
+
+function mpu_reading() {
+  var m6 = mpu.getMotion6();
+  var gyro_divider = GYRO_DIVIDERS[mpu.getFullScaleGyroRange()];
+  var accel_divider = ACCEL_DIVIDERS[mpu.getFullScaleAccelRange()];
+  var date = date_string_get(new Date());
+  var ori = {
+    alpha: m6[3] / gyro_divider,
+    beta: m6[4] / gyro_divider,
+    gamma: m6[5] / gyro_divider,
+    time: date
+  };
+  var mot = {
+    gacc: {
+      x: m6[0] / accel_divider,
+      y: m6[1] / accel_divider,
+      z: m6[2] / accel_divider,
+    },
+    time: date
+  };
+
+  fs.appendFileSync("./data/ori.log", JSON.stringify(ori) + "\n");
+  fs.appendFileSync("./data/mot.log", JSON.stringify(mot) + "\n");
 }
 
 mpu_start();
@@ -83,11 +116,17 @@ app.get("/data", http_get_data);
 function http_get_data(req, res) {
   var data = {};
 
-  if (fs.existsSync("./data/acc.log")) {
-    var acc = fs.readFileSync("./data/acc.log", { encoding: "utf8" });
-    accs = acc.trim().split("\n");
-    data.acc = accs;
-    fs.unlinkSync("./data/acc.log");
+  if (fs.existsSync("./data/mot.log")) {
+    var mot = fs.readFileSync("./data/mot.log", { encoding: "utf8" });
+    mots = mot.trim().split("\n");
+    data.mot = mots;
+    fs.unlinkSync("./data/mot.log");
+  }
+  if (fs.existsSync("./data/ori.log")) {
+    var ori = fs.readFileSync("./data/ori.log", { encoding: "utf8" });
+    oris = ori.trim().split("\n");
+    data.ori = oris;
+    fs.unlinkSync("./data/ori.log");
   }
   if (fs.existsSync("./data/geo.log")) {
     var geo = fs.readFileSync("./data/geo.log", { encoding: "utf8" });
