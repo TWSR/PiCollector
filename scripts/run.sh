0173388e7cc0efc638b5613a94fdd5aa91b6c418
unknown <cytechtw@gmail.com>
2018-08-01 03:45:11 +0800

    add sudo for change led

--- scripts/run.sh
+++ scripts/run.sh
@@ -8,6 +8,6 @@ $BASEDIR/scripts/privilege.sh
 su $USER -c " \
   cd $BASEDIR; \
   printf '$DATE\\tPiCollector Start ... \\n' | tee -a data/run.log; \
-  npm start > data/npm_start.log 2>&1 &
+  sudo npm start > data/npm_start.log 2>&1 &
 "
 
