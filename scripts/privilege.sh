eaa7b39ad93102315b5c289559fcc523afe7fce0
unknown <cytechtw@gmail.com>
2018-08-01 03:41:14 +0800

    chmod with led1

--- scripts/privilege.sh
+++ scripts/privilege.sh
@@ -9,4 +9,5 @@ chmod a+rw /dev/i2c-1
 chmod a+rw /dev/ttyUSB0
 chmod a+rw /sys/class/leds/led0/trigger
 chmod a+rw /sys/class/leds/led0/brightness
-
+chmod a+rw /sys/class/leds/led1/trigger
+chmod a+rw /sys/class/leds/led1/brightness
