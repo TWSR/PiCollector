02cd7fc8e8b0fb29aa7d1cdee489bcce719c4e8c
Audi Hsu <audihsu@gmail.com>
2018-08-04 17:28:26 +0000

    modify scripts

--- scripts/privilege.sh
+++ scripts/privilege.sh
@@ -5,8 +5,8 @@
   exit 1
 }
 
-chmod a+rw /dev/i2c-1
-chmod a+rw /dev/ttyUSB0
+[ -e /dev/i2c-1 ] && chmod a+rw /dev/i2c-1
+[ -e /dev/ttyUSB0 ] && chmod a+rw /dev/ttyUSB0
 chmod a+rw /sys/class/leds/led0/trigger
 chmod a+rw /sys/class/leds/led0/brightness
 chmod a+rw /sys/class/leds/led1/trigger
